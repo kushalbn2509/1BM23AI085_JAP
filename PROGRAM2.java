5f61262c656dcc64ca1f53affe30dae9c2258c24
kushalbn2509 <kushalbn.ai23@bmsce.ac.in>
2025-03-12 15:54:54 +0530

    Add files via upload

---
+++ PROGRAM2.java
@@ -0,0 +1,34 @@
+public class PROGRAM2
+{
+    public static void main(String args[])
+    {
+        int a=5;
+        Integer b=a;
+        int c=b ;
+        System.out.println("Value of a = "+a);
+        System.out.println("Value of b = "+b);
+        System.out.println("Value of c = "+c);
+
+        double x=5.0;
+        Double y=x;
+        double z=y ;
+        System.out.println("Value of x = "+x);
+        System.out.println("Value of y = "+y);
+        System.out.println("Value of z = "+z);
+
+        byte x1=5;
+        Byte y1=x1;
+        byte z1=y1 ;
+        System.out.println("Value of x1 = "+x1);
+        System.out.println("Value of y1 = "+y1);
+        System.out.println("Value of z1 = "+z1);
+
+        char x2='a';
+        Character y2=x2;
+        char z2=y2 ;
+
+        System.out.println("Value of x2 = "+x2);
+        System.out.println("Value of y2 = "+y2);
+        System.out.println("Value of z2 = "+z2);
+    }
+}
